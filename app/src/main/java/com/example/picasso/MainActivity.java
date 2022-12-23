a237cb3742f2f6bf0db4280d7f51c9da750ad7bd
Reyhaneh <reyhanehahmadi8203@gmail.com>
2022-12-23 19:42:23 +0330

    first commit

---
+++ app/src/main/java/com/example/picasso/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.picasso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView img1, img2, img3, img4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        img1=(ImageView) findViewById(R.id.img1);
+        img2=(ImageView) findViewById(R.id.img2);
+        img3=(ImageView) findViewById(R.id.img3);
+        img4=(ImageView) findViewById(R.id.img4);
+
+        Picasso.get().load("https://images.unsplash.com/photo-1518802508264-76256089cddb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2487&q=80").into(img1);
+        Picasso.get().load("https://images.unsplash.com/photo-1513986615308-8f59cc8a16e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80").into(img2);
+        Picasso.get().load("https://images.unsplash.com/photo-1445503061048-e03cd48b1f11?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80").into(img3);
+        Picasso.get().load("https://images.unsplash.com/photo-1467649165350-bdae46d9779b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80").into(img4);
+    }
+}
